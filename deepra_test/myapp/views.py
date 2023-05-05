2ce28cc89890465cd81e32642040e477a1ab7478
Александр <timofeev2.as@edu.spbstu.ru>
2023-05-05 21:20:45 +0300

    greeting index page

--- deepra_test/myapp/views.py
+++ deepra_test/myapp/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-def hello(request):
-    name = request.GET.get('name')
-    message = request.GET.get('message')
-    return HttpResponse(f'Hello {name}!\n{message}!')
+
+def index(request):
+    name = request.GET.get('name', '')
+    message = request.GET.get('message', '')
+    return render(request, 'index.html', {'name': name, 'message': message})
