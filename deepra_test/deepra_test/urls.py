d2887da448850d8d046d34f2438760941d15ed90
Александр <timofeev2.as@edu.spbstu.ru>
2023-05-05 21:14:16 +0300

    init greeting request

--- deepra_test/deepra_test/urls.py
+++ deepra_test/deepra_test/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+from myapp.views import hello
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', hello, name='hello'),
 ]
--- deepra_test/myapp/views.py
+++ deepra_test/myapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def hello(request):
+    name = request.GET.get('name')
+    message = request.GET.get('message')
+    return HttpResponse(f'Hello {name}!\n{message}!')
