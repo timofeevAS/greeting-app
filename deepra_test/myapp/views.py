269d3df67c6e7721bc11c79e9ca90f0d192c0368
Александр <timofeev2.as@edu.spbstu.ru>
2023-05-05 21:49:05 +0300

    views get request changes (if name or message is None)

--- deepra_test/myapp/views.py
+++ deepra_test/myapp/views.py
@@ -4,6 +4,12 @@ from django.http import HttpResponse
 
 
 def index(request):
-    name = request.GET.get('name', '')
-    message = request.GET.get('message', '')
+    name = request.GET.get('name', None)
+    message = request.GET.get('message', None)
+
+    if name is None:
+        name = 'Deppra'
+    if message is None:
+        message = 'Let\'s go be friends!'
+
     return render(request, 'index.html', {'name': name, 'message': message})
